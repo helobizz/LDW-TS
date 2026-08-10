8d6e06bd5f60a350f17046ee7e9366162461c004
DSM4 <fatec-dsm4@academico.intra>
2026-08-10 19:16:40 -0300

    Atualizando aula

---
+++ aula01/01_typescript_basico.ts
@@ -0,0 +1,10 @@
+// Tipagem Implicita
+let nomeEstudante = "Heloísa";
+//nomeEstudante = 8;
+
+// Tipagem Explicita
+let matriculado: boolean = true;
+let idade: number = 20;
+let habilidades: string[] = ["HTML", "CSS", "JavaScript"];
+let habilidades2: Array<String> = ["HTML", "CSS", "JavaScript"];
+let notas: Array<number> = [8.5, 9.0, 7.5]
